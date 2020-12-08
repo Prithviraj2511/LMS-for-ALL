a2f74130039347c1c297083c3a7fdc714461b11f
Prithviraj2511 <prithvirajpatil2511@gmail.com>
2020-12-08 13:59:46 +0530

    connection done

--- Templates/owner/connect.php
+++ Templates/owner/connect.php
@@ -1,7 +1,7 @@
 <?php
 
 require '../../vendor/autoload.php';
-$client=new MongoDB\Client;
+$client=new MongoDB\Client('mongodb+srv://TaskApp:raj12345patil@cluster0.ypfhs.mongodb.net/sotrage?retryWrites=true&w=majority');
 
 $db=$client->storage;
 
--- connectDatabase.php
+++ connectDatabase.php
@@ -1,6 +1,6 @@
 <?php
         require "vendor/autoload.php";
-        $client = new MongoDB\Client;
+        $client = new MongoDB\Client('mongodb+srv://TaskApp:raj12345patil@cluster0.ypfhs.mongodb.net/storage?retryWrites=true&w=majority');
         $db = $client->storage;
         if(!$db){
                 die('Could not connect :');
